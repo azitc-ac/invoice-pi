92424b817308bc1e6ce9ca5775b03aecad1525a8
azitc-ac <alexander@zarenko.net>
2026-03-17 12:27:24 +0100

    Update analyze.py
    improved detection

--- app/flows/analyze.py
+++ app/flows/analyze.py
@@ -83,6 +83,7 @@ DATE_FALLBACK_PATTERNS = [
 INVOICE_NR_PATTERNS = [
     r"Rechnungsnummer[:\s#]+([A-Z0-9\-\/]{4,30})",
     r"Rechnungs-?Nr\.?[:\s#]+([A-Z0-9\-\/]{4,30})",
+    r"Rechnung-?Nr\.?[:\s#]+([A-Z0-9\-\/]{4,30})",
     r"Invoice\s*(?:No|Number|Nr)\.?[:\s#]+([A-Z0-9\-\/]{4,30})",
     r"Beleg(?:nummer)?[:\s#]+([A-Z0-9\-\/]{4,30})",
     r"(?:No|Nr)\.[:\s]+([A-Z0-9\-\/]{4,30})",
